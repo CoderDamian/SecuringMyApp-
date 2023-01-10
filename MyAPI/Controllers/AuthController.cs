85b2bbd94cfad04e0ba1ccf4b9504650c38b3341
Damian <AdrianAndradeChristian@outlook.com>
2023-01-09 21:26:21 -0500

    Implementing JWT Authentication

---
+++ MyAPI/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MyAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody]LoginModel user)
+        {
+            if (user is null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            if (user.UserName.Equals("jhondoe") && user.Password.Equals("def@123"))
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:5001",
+                    audience: "https://localhost:5001",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+                    );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+                return Ok(new AuthenticatedResponse { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ MyAPI/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet, Authorize]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "John Doe", "Jane Doe" };
+        }
+    }
+}
